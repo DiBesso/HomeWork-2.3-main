85eb7cde2aba1fd68af29f4532616277cd5e6073
Dmitry <radiator211@mail.ru>
2021-12-15 02:53:10 +0700

    Work with Outlet

--- HomeWork 2.3/ViewController.swift
+++ HomeWork 2.3/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var LogInButton: UIButton!
+    @IBOutlet weak var FargotUserNameButton: UIButton!
+    @IBOutlet weak var FargotPassButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
