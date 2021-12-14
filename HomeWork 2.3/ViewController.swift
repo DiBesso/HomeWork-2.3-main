f52f606fcc43e1b3b8263f84ee10d52d26a82643
Dmitry <radiator211@mail.ru>
2021-12-15 02:48:38 +0700

    Initial Commit

---
+++ HomeWork 2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork 2.3
+//
+//  Created by Дмитрий Бессонов on 15.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HomeWork 2.3/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  HomeWork 2.3
+//
+//  Created by Дмитрий Бессонов on 15.12.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
