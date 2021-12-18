57f72c58346824cbe474f16ba7158043dfbcc8de
Dmitry <radiator211@mail.ru>
2021-12-18 16:20:49 +0700

    Work with Facts

--- HomeWork 2.3/Models/FactsAboutMe.swift
+++ HomeWork 2.3/Models/FactsAboutMe.swift
@@ -9,12 +9,27 @@ import UIKit
 
 struct User {
     let name: String
+    let password: String
     let surname: String
     let age: Int
     let person: [Facts]
 }
 
 struct Facts {
+    let numberOfFact: NumbersOfFact
+}
+
+enum NumbersOfFact: String {
+    case one = "Я родился и живу в городе - Новосибирск"
+    case two = "Я работаю в компании Nike"
+    case three = "Я учусь новой професси - iOS-разработчик"
+    case four = "Мне тяжело даются домашки, но я разбирусь и стану успешным программистом!"
     
+}
+
+func NumberOneFactAlert () {
+    let alert = UIAlertController (title: "Факт 1:", message: "Я родился и живу в городе - Новосибирск", preferredStyle: UIAlertController.Style.alert)
+    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil))
+    present (alert, animated: true)
     
 }
